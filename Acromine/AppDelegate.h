98297e9b89cec2b71e0bf8f986862aec0b98fd89
DevanRaju <devanraju@administrators-mac-mini.local>
2016-01-08 01:25:56 +0530

    Initial Commit

---
+++ Acromine/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Acromine
+//
+//  Created by DevanRaju on 08/01/16.
+//  Copyright © 2016 First-tek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Acromine/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Acromine
+//
+//  Created by DevanRaju on 08/01/16.
+//  Copyright © 2016 First-tek. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
